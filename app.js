33bd13c1bc3cfa3fd3aa35d3341eb928a1296e6a
jzGreen <a@abemassry.com>
2013-11-26 14:44:28 +1100

    console logging to find out more about data

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ var ninja = ninjaBlocks.app({user_access_token:USER_ACCESS_TOKEN});
 // then analyze and get a single one
 ninja.devices({ device_type: 'temperature' }, function(err, devices) {
   _.each(devices, function(device,guid){
-    ninja.device(guid).last_heartbeat(function(err, data) { 
+    ninja.device(guid).last_heartbeat(function(err, data) {
+      console.log('the guid is: '+guid);
+      console.log('');
       console.log(device.shortName+' is '+data.DA+'C');
+      console.log('');
+      console.log('the data is: '+data);
     })
   })
 });
