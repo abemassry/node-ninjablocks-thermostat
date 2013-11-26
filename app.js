81ce99c0887d2d23be2d270a375f5c41ee0c8726
jzGreen <a@abemassry.com>
2013-11-26 14:39:46 +1100

    adding ninjablocks and generalized access token

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var USER_ACCESS_TOKEN = vars.userAccessToken;
 var ninja = ninjaBlocks.app({user_access_token:USER_ACCESS_TOKEN});
 
 // Get the most recent temperature reading from all temperature sensors
+// then analyze and get a single one
 ninja.devices({ device_type: 'temperature' }, function(err, devices) {
   _.each(devices, function(device,guid){
     ninja.device(guid).last_heartbeat(function(err, data) { 
