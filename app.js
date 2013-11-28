eaa1d2a092368bf88204ec3a1e1eaa7535f4a3df
jzGreen <a@abemassry.com>
2013-11-28 10:07:53 -0500

    set constants

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ var ninja = ninjaBlocks.app({user_access_token:USER_ACCESS_TOKEN});
 
 var MAX_TIME_DELTA = 180;
 var GUID_1 = '0101';
+var START_TIME = 22;
+var END_TIME = 7;
+var LOW_LIMIT_HEAT = 68;
+var HIGH_LIMIT_HEAT = 72;
+var HIGH_LIMIT_AC = 74;
 
 // Get the most recent temperature reading from all temperature sensors
 // then analyze and get a single one
@@ -32,14 +37,14 @@ ninja.devices({ device_type: 'temperature' }, function(err, devices) {
           var now = moment();
           var hour = now.hours();
           console.log(hour);
-          if (hour > 22 || hour < 7) {
-            if (temp_f < 68) {
+          if (hour > START_TIME || hour < END_TIME) {
+            if (temp_f < LOW_LIMIT_HEAT) {
               // set to heat
               // check temp and raise by 1 degree
-            } else if(temp > 72) {
+            } else if(temp > HIGH_LIMIT_HEAT) {
               // check temp and lower by 1 degree
 
-              if (temp > 74) {
+              if (temp > HIGH_LIMIT_AC) {
                 // switch to air conditioning
                 // check temp and lower by 1 degree
 
